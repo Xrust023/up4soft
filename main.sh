a54297a3b24b1a095a5c70db447dbaaf3a75ef8f
Dimon <gameguru2005@gmail.com>
2024-09-05 20:23:14 +0300

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,36 @@
+#!bin/bash
+
+sudo apt update
+
+sudo apt install nginx -y
+
+sudo apt install php-fpm php-mysql php-pgsql php-xml php-mbstring php-curl php-zip php-gd -y
+
+#Установка Wordpress
+sudo wget https://wordpress.org/latest.tar.gz
+sudo rm -rf /var/www/html/wordpress
+sudo mkdir /var/www/html/wordpress
+sudo tar -xzvf latest.tar.gz
+sudo mv wordpress/* /var/www/html/wordpress
+sudo rm -rf latest.tar.gz
+
+#Установка конфигурации для PostgreSql
+sudo git clone https://github.com/kevinoid/postgresql-for-wordpress.git /var/www/html/wordpress/wp-content/plugins/postgresql-for-wordpress
+sudo mv postgresql-for-wordpress/pg4wp pg4wp
+sudo rm -rf /var/www/html/wordpress/wp-content/plugins/postgresql-for-wordpress
+sudo cp pg4wp/db.php db.php
+
+#Установка нужных файлов
+sudo rm /var/www/html/wordpress/wp-config-sample.php
+sudo cp up4soft/wp-config.php /var/www/html/wordpress
+sudo systemctl stop nginx && sudo rm /etc/nginx/sites-avalable/default && sudo cp up4soft/default /etc/nginx/sites-available/
+sudo mv up4soft/main.html /var/www/html/
+sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+
+# Права на файлы
+sudo chown -R www-data:www-data /var/www/html/wordpress/
+sudo find /var/www/html/wordpress/ -type d -exec chmod 755 {} \;
+sudo find /var/www/html/wordpress/ -type f -exec chmod 644 {} \;
+
+
+sudo systemctl start nginx
